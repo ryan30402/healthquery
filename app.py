09d0f7bec44c8d9668791416e5b63f43331fb93f
ryan30402 <ryancw.888@gmail.com>
2026-09-16 17:03:04 -0400

    Validate empty and whitespace-only questions

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 print("Welcome to HealthQuery!")
 
 question = input("Enter a medical information question: ")
-
-print("Your question:", question)
+question = question.strip()
+if question == "":
+    print("Please enter a medical information question.")
+else:
+    print("Your question:", question)
